6ad379bb23fca4b6ec13d029096665e92d58b023
froja <sps4daniil@gmail.com>
2025-05-05 07:34:35 +0300

    deploy: подготовка конфига к деплою в docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-})
+export default ({mode}) => {
+    // eslint-disable-next-line no-undef
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    return defineConfig({
+        plugins: [react()],
+        // eslint-disable-next-line no-undef
+        base: process.env['VITE_BASE_PATH'],
+    });
+}
