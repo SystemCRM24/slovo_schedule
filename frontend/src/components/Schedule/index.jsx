36c7fbd619539941a9425282d3f2ecce7af60b83
froja <sps4daniil@gmail.com>
2025-05-05 11:50:08 +0300

    feat(schedule): получение расписания и сотрудников из Б24

--- frontend/src/components/Schedule/index.jsx
+++ frontend/src/components/Schedule/index.jsx
@@ -2,7 +2,7 @@ import React, {Suspense, useEffect, useState, useMemo} from 'react';
 import {Spinner, Table} from "react-bootstrap";
 
 import DaySchedule from '../DaySchedule';
-import {clientMock} from "../../api/";
+import apiClient, {clientMock} from "../../api/";
 import {useScheduleContext} from "../../contexts/Schedule/provider.jsx";
 import {useWorkScheduleContext} from "../../contexts/WorkSchedule/provider.jsx";
 import {SpecialistContextProvider} from "../../contexts/Specialist/provider.jsx";
@@ -15,11 +15,13 @@ const Schedule = ({fromDate, toDate}) => {
     const [schedule, setSchedule] = useScheduleContext();
     const [workSchedule, setWorkSchedule] = useWorkScheduleContext();
 
-    const apiClient = clientMock;
+    // const apiClient = apiClient;
 
     useEffect(() => {
         (async () => {
-            setSpecialists(await apiClient.getSpecialists());
+            const allSpecialists = await apiClient.getSpecialists();
+            console.log(allSpecialists);
+            setSpecialists(allSpecialists);
         })();
     }, []);
 
@@ -27,14 +29,14 @@ const Schedule = ({fromDate, toDate}) => {
         (async () => {
             if (fromDate && toDate) {
                 const [scheduleData, workScheduleData] = await Promise.all([
-                    apiClient.getSchedule(fromDate, toDate),
-                    apiClient.getWorkSchedule(fromDate, toDate)
+                    apiClient.getSchedules(fromDate, toDate),
+                    apiClient.getWorkSchedules(fromDate, toDate)
                 ]);
                 setSchedule(scheduleData);
                 setWorkSchedule(workScheduleData);
             }
         })();
-    }, [fromDate, toDate]);
+    }, [fromDate, setSchedule, setWorkSchedule, toDate]);
 
     const headers = useMemo(
         () => {
