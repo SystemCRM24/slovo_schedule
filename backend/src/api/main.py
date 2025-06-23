a7086c12428d6adf0af213d806caf24b7745a229
Ilya <ilyxaz68@mail.ru>
2025-06-23 16:56:59 +0300

    added new route in api/main.py. Its return data(BXContacts) from bx. Also added tests for new route

--- backend/src/api/main.py
+++ backend/src/api/main.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends
+from typing import Dict
+from src.utils import extract
 
-from src.core import BitrixClient
+from src.core import BitrixClient, BXConstants
 from src.schemas.api import (
     BXSpecialist, 
     BXClient, 
@@ -42,3 +44,21 @@ async def get_work_schedules(query: QueryDateRange = Depends()) -> list[BXSchedu
     bx_schedules = map(lambda s: BXSchedule(**s), schedules)
     filtered = filter(lambda s: s.is_valid(), bx_schedules)
     return list(filtered)
+
+
+@router.get("/get_constants", status_code=200)
+async def get_constants() -> Dict:
+    """Возвращает содержимое BXConstants в виде словаря."""
+    bx_dict = {}
+
+    for key in dir(BXConstants):
+        if key.startswith('__'):
+            continue
+        value = getattr(BXConstants, key)
+
+        if isinstance(value, type):
+            bx_dict[key] = extract(value)
+        elif isinstance(value, dict):
+            bx_dict[key] = value
+
+    return bx_dict
