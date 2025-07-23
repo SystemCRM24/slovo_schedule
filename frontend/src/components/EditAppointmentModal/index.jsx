8ea41414a983edce7a5340f2fdb0ab4922ab688b
heccberte <145110913+heccberte@users.noreply.github.com>
2025-07-23 14:45:11 +0300

    фикс сохранения изменений клиента

--- frontend/src/components/EditAppointmentModal/index.jsx
+++ frontend/src/components/EditAppointmentModal/index.jsx
@@ -218,7 +218,7 @@ const EditAppointmentModal = ({ id, show, setShow, startDt, endDt, patientId, pa
             handleClose={() => setShow(false)}
             title={`${specialist.name} - ${patientName} ${patientType} ${dayOfWeek} ${dateString} ${getTimeStringFromDate(startDt)} - ${getTimeStringFromDate(endDt)}`}
             primaryBtnDisabled={!isNewScheduleIntervalValid(appointment, scheduleWithoutCurrentElem, scheduleWithoutCurrentElem, workSchedule.intervals)}
-            handlePrimaryBtnClick={onSubmit}
+            handlePrimaryBtnClick={() => onSubmit(appointment)}
             primaryBtnText={'Сохранить'}
         >
             <div className="d-flex flex-column align-items-center justify-content-center w-100 h-100 gap-2">
