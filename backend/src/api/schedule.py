4001fe8f0a9a6b94918aea155f005045964c0b41
dfx_17 <rossohinpavel@yandex.ru>
2025-08-08 15:35:27 +0300

    ggg

--- backend/src/api/schedule.py
+++ backend/src/api/schedule.py
@@ -90,8 +90,8 @@ async def update_schedule_massive(id: int, schedule: Schedule, bt: BackgroundTas
         schedule_date = s.get(BXConstants.schedule.uf.date, None)
         if not schedule_date:
             continue
-        print('NOnE is skipping')
         schedule_date = datetime.fromisoformat(schedule_date).replace(tzinfo=Settings.TIMEZONE)
+        print(start, schedule_date, start.weekday(), schedule_date.weekday())
         if schedule_date.weekday() != start.weekday():
             continue
         print('im HERE')
