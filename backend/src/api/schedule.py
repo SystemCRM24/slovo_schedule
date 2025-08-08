62a3007b4539f97ad0cb54f33d9a7599c3cddfb4
dfx_17 <rossohinpavel@yandex.ru>
2025-08-08 17:09:17 +0300

    ggg

--- backend/src/api/schedule.py
+++ backend/src/api/schedule.py
@@ -76,11 +76,11 @@ async def update_schedule(id: int, schedule: Schedule, bt: BackgroundTasks) -> S
 @router.put('/massive/{id}', status_code=200)
 async def update_schedule_massive(id: int, schedule: Schedule, bt: BackgroundTasks):
     """Массовое обновление графиков."""
+    print(datetime.fromisoformat(schedule.date))
     start = datetime.fromisoformat(schedule.date).replace(tzinfo=Settings.TIMEZONE)
-    start = start - timedelta(days=1)
-    end = start + timedelta(days=365)
+    end = start
     schedules = await BitrixClient.get_specialists_schedules(
-        start.isoformat(),
+        (start - timedelta(days=1)).isoformat(),
         end.isoformat(),
         (schedule.specialist, )
     )
