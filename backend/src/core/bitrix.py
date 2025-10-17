cc676777bd2b17be5c51e58b9a9da738dc523c5b
dfx_17 <rossohinpavel@yandex.ru>
2025-10-17 17:53:59 +0300

    check

--- backend/src/core/bitrix.py
+++ backend/src/core/bitrix.py
@@ -36,13 +36,13 @@ class BitrixClient:
         if dct:
             requests.append(dct)
         list_result, dict_result = [], {}
-        print(len(requests))
         for request in requests:
             middle_result = await BITRIX.call_batch({'halt': 0, 'cmd': request})
             if isinstance(middle_result, list):
                 list_result.extend(middle_result)
             if isinstance(middle_result, dict):
                 dict_result |= middle_result
+        print('[debug]', len(list_result), len(dict_result))
         return list_result if list_result else dict_result
 
     # Методы для CRUD-функционала
