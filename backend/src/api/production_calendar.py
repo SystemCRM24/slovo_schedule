2c9c18d438da6b21e060f34d07fd18e1d6269be6
dfx_17 <rossohinpavel@yandex.ru>
2025-10-31 13:40:18 +0300

    change production calendar method to retreive holidays

--- backend/src/api/production_calendar.py
+++ backend/src/api/production_calendar.py
@@ -7,19 +7,16 @@ from src.core import BitrixClient
 router = APIRouter(prefix='')
 
 
-@router.get('/production_calendar')
-async def get_production_calendar(query: RangeQuery = Depends()) -> list[date]:
-    """Метод для получения производственного календаря. 
-    Возращает массив строк, где каждая строка - дата без времени.
-    Строится на основе каленадря битрикса id=3
-    """
+@router.get('/get_holidays')
+async def get_holidays(query: RangeQuery = Depends()) -> list[date]:
+    """Метод для получения списка выходных, основанном на производственном календаре"""
     raw_calendar = await BitrixClient.get_production_calendar()
     marked_holidays = raw_calendar.get('CALENDAR', {}).get('EXCLUSIONS', {})
     result = []
     while query.start <= query.end:
         year, month, day = map(str, (query.start.year, query.start.month, query.start.day))
         is_holiday = marked_holidays.get(year, {}).get(month, {}).get(day, None)
-        if is_holiday is None:
+        if is_holiday is not None:
             result.append(query.start)
         query.start += timedelta(days=1)
     return result
